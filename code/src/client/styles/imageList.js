119852df14e27627a918aa3169d12100dca5ed8c
salhans <shagun.alhans@soprasteria.com>
2020-07-21 17:54:02 +0000

    remove extra

--- code/src/client/styles/imageList.js
+++
@@ -1,8 +0,0 @@
-export const imageListStyles = {
-   imgGrid: {
-    maxHeight: '100vh', overflow: 'auto'
-   },
-   imgCardHeight: { height: '350px' },
-   selectedImageGrid: { padding: '1%', paddingTop: '12%', backgroundColor: 'beige', maxHeight: '100vh' },
-   clearBtn: { padding: '5%', paddingLeft: '42%' }
-};
